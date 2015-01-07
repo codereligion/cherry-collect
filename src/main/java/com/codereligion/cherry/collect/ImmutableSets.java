5c7383502594b21a9a08e8017ba13994e97d6c53
Sebastian Gröbler <s.groebler@googlemail.com>
2015-01-07 10:17:42 +0100

    added immutable set factory

---
+++ src/main/java/com/codereligion/cherry/collect/ImmutableSets.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright 2014 www.codereligion.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codereligion.cherry.collect;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * Provides static factory methods to create {@link com.google.common.collect.ImmutableSet ImmutableSets}.
+ *
+ * @author Sebastian Gr&oml;bler
+ * @since 28.12.2014
+ */
+public final class ImmutableSets {
+
+    private ImmutableSets() {
+        throw new IllegalAccessError("This class is a utility class and must not be instantiated.");
+    }
+
+    /**
+     * TODO
+     *
+     * @param iterable the entries to be transformed
+     * @param function the function to transform the entries with
+     * @param <F>      the type of the entries to be transformed
+     * @param <T>      the type of the resulting entries
+     * @return a {@link com.google.common.collect.ImmutableSet}, might be empty
+     * @throws IllegalArgumentException when any of the given parameters are {@code null}
+     */
+    public static <F, T> ImmutableSet<T> from(final Iterable<F> iterable, final Function<? super F, T> function) {
+
+        checkArgument(iterable != null, "iterable must not be null.");
+        checkArgument(function != null, "function must not be null.");
+
+        final ImmutableSet.Builder<T> builder = ImmutableSet.builder();
+
+        for (final F f : iterable) {
+            final T t = function.apply(f);
+            builder.add(t);
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * TODO
+     *
+     * @param iterable  the entries to be filtered
+     * @param predicate the predicate to filter the entries with
+     * @param <E>       the type of the entries to be filtered
+     * @return a {@link com.google.common.collect.ImmutableSet}, might be empty
+     * @throws IllegalArgumentException when any of the given parameters are {@code null}
+     */
+    public static <E> Set<E> from(final Iterable<E> iterable, final Predicate<? super E> predicate) {
+
+        checkArgument(iterable != null, "iterable must not be null.");
+        checkArgument(predicate != null, "predicate must not be null.");
+
+        final ImmutableSet.Builder<E> builder = ImmutableSet.builder();
+
+        for (final E e : iterable) {
+            if (predicate.apply(e)) {
+                builder.add(e);
+            }
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * TODO
+     *
+     * @param iterable  the entries to be filtered and transformed
+     * @param predicate the predicate to filter the entries with
+     * @param function  the function to transform the entries with
+     * @param <F>       the type of the entries to be transformed
+     * @param <T>       the type of the resulting entries
+     * @return a {@link com.google.common.collect.ImmutableSet}, might be empty
+     * @throws IllegalArgumentException when any of the given parameters are {@code null}
+     */
+    public static <F, T> ImmutableSet<T> from(final Iterable<F> iterable, final Predicate<? super F> predicate, final Function<? super F, T> function) {
+
+        checkArgument(iterable != null, "iterable must not be null.");
+        checkArgument(predicate != null, "predicate must not be null.");
+        checkArgument(function != null, "function must not be null.");
+
+        final ImmutableSet.Builder<T> builder = ImmutableSet.builder();
+
+        for (final F f : iterable) {
+            if (predicate.apply(f)) {
+                final T t = function.apply(f);
+                builder.add(t);
+            }
+        }
+
+        return builder.build();
+    }
+
+}
---
+++ src/test/java/com/codereligion/cherry/collect/ImmutableSetsTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2014 www.codereligion.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codereligion.cherry.collect;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+
+/**
+ * Tests {@link ImmutableSets} contract.
+ *
+ * @author Sebastian Gr&ouml;bler
+ * @since 29.12.2014
+ */
+public class ImmutableSetsTest extends AbstractIterableFactoryMethodTest {
+
+    @Override
+    protected Iterable<Integer> from(final Iterable<Integer> iterable, final Predicate<Integer> predicate) {
+        return ImmutableSets.from(iterable, predicate);
+    }
+
+    @Override
+    protected Iterable<String> from(final Iterable<Integer> iterable, final Function<Integer, String> function) {
+        return ImmutableSets.from(iterable, function);
+    }
+
+    @Override
+    protected Iterable<String> from(final Iterable<Integer> iterable, final Predicate<Integer> predicate, final Function<Integer, String> function) {
+        return ImmutableSets.from(iterable, predicate, function);
+    }
+
+    @Override
+    protected Class<?> getFactoryClass() {
+        return ImmutableSets.class;
+    }
+}
